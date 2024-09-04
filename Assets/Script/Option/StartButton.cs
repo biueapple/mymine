597e1c6f0d629bd0bd7f3bd8a2299b207403c1e4
DESKTOP-TTSUQC3\duqor <duqorghg@gmail.com>
2024-09-04 15:12:06 +0900

    동굴을 만들기 위한 알고리즘 Wrom 알고리즘을 간단하게 만들고 테스트를 위한 코드 추가

--- Assets/Script/Option/StartButton.cs
+++ Assets/Script/Option/StartButton.cs
@@ -1,10 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public GameObject cube1;
+
+    public GameObject cube2;
+
+    private void Start()
+    {
+        List<(Vector3Int, int)> dir = new()
+        {
+            (new Vector3Int(1, 0, 0), 4),
+            (new Vector3Int(-1, 0, 0), 4),
+            (new Vector3Int(0, 0, 1), 4),
+            (new Vector3Int(0, 0, -1), 4),
+            (new Vector3Int(0, -1, 0), 8),
+            (new Vector3Int(0, 1, 0), 1)
+        };
+        Worm worm = Worm_Algorithm.Instance.Start(new Vector3Int(0,0,0), dir);
+    
+        for(int i = 0; i < worm.pathRange.Count;i++)
+        {
+            if (!worm.path.Contains(worm.pathRange[i]))
+                Instantiate(cube2, worm.pathRange[i], Quaternion.identity);
+        }
+
+        for (int i = 0; i < worm.path.Count; i++)
+        {
+            Instantiate(cube1, worm.path[i], Quaternion.identity);
+        }
+    }
+
     public void OnStartButton()
     {
         GameManager.Instance.Initialization();
---
+++ Assets/Script/World/Worm_Algorithm.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class Worm_Algorithm
+{
+    private static Worm_Algorithm instance;
+    public static Worm_Algorithm Instance 
+    { get 
+        {
+            if (instance == null)
+                instance = new Worm_Algorithm();
+            return instance;
+        } 
+    }
+
+    public Worm Start(Vector3Int position, List<(Vector3Int, int)> dir)
+    {
+        Worm worm = new Worm(10, 2, 2, 2, dir);
+        worm.Start();
+
+        worm.PathRange(position);
+
+        return worm;
+    }
+    
+}
+
+public class Worm
+{
+    //size
+    public int length;
+    public int w;
+    public int h;
+    public int d;
+
+    //움직일 수 있는 방향과 확률
+    public List<(Vector3Int, int)> dir = new ();
+    public List<Vector3Int> range = new();
+    public List<Vector3Int> path = new() ;
+    public List<Vector3Int> pathRange = new();
+
+    public Worm(int length, int w, int h, int d, List<(Vector3Int, int)> values)
+    {
+        this.length = length;
+        this.w = w;
+        this.h = h;
+        this.d = d;
+        dir = values;
+        Init();
+    }
+
+    public void Start()
+    {
+        Vector3Int dir;
+        path.Add(new Vector3Int(0, 0, 0));
+        while (length > 0)
+        {
+            dir = Direction();
+
+            path.Add(path[^1] + dir);
+
+            length--;
+        }
+    }
+
+    public void PathRange(Vector3Int position)
+    {
+        for (int i = 0; i < path.Count; i++)
+        {
+            Range(position + path[i]);
+        }
+    }
+
+    //움직일 방향을 리턴해줌
+    public Vector3Int Direction()
+    {
+        int value = Random.Range(0, dir.Sum(x => x.Item2) + 1);
+        float cumulative = 0f;
+
+        foreach (var (direction, probability) in dir)
+        {
+            cumulative += probability;
+            if (value < cumulative)
+            {
+                return direction;
+            }
+        }
+
+        return Vector3Int.zero;
+    }
+
+    public void Range(Vector3Int vector)
+    {
+        for (int i = 0; i < range.Count; i++)
+        {
+            if(!pathRange.Contains(range[i] + vector))
+                pathRange.Add(range[i] + vector);
+        }
+    }
+
+    public void Init()
+    {
+        for(int x = -w + 1; x < w; x++)
+        {
+            for (int y = -h + 1; y < h; y++)
+            {
+                for (int z = -d + 1; z < d; z++)
+                {
+                    range.Add(new Vector3Int (x, y, z));
+                }
+            }
+        }
+    }
+}
