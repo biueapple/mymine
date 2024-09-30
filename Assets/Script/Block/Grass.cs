c1fc2e463a913e1106c5bff0a6fd003611a278f2
DESKTOP-TTSUQC3\duqor <duqorghg@gmail.com>
2024-09-30 14:18:22 +0900

    기존에 있던 사용아이템 효과는 없던게 맞고 Block쪽의 문제 때문에 경작지로의 변환이 안됐었음 Dirt에만 경작지로 바뀌는 효과가 있고 Grass에는 없던게 문제

--- Assets/Script/Block/Grass.cs
+++ Assets/Script/Block/Grass.cs
@@ -2,10 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grass : Block
+public class Grass : Block, IBlockActive
 {
     public Grass(int id) : base(id, "잔디", HardnessType.SHOVELS, 1, false, true, 5, new int[6] { 332, 332, 602, 200, 332, 332 })
     {
 
+    }
+
+    public void Active(Item item, World.BlockLaycast blockLaycast)
+    {
+        //만약 삽으로 우클릭 했다면 경작지로 변환
+        if (item is Strength_Shovels shovels)
+        {
+            Debug.Log("삽으로 흙을 우클릭 경작지로 변환");
+            World.Instance.WorldPositionEdit(blockLaycast.positionToInt, 5);
+            if (item is IConsume consume)
+            {
+                consume.Consume();
+                Debug.Log("남은 내구도 " + consume.Durability);
+            }
+        }
     }
 }
